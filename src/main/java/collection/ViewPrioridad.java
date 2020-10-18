712193c07d31623a49915a1f4f20e765686b5eb9
Karina <kserranoa16@gmail.com>
2020-10-18 16:30:12 -0600

    Metodos para manejo de tiquetes

--- src/main/java/collection/ViewPrioridad.java
+++ src/main/java/collection/ViewPrioridad.java
@@ -8,7 +8,7 @@ public class ViewPrioridad implements Comparator<InfoTiquete> {
     
     @Override
     public int compare(InfoTiquete o1, InfoTiquete o2) {
-        return o1.getPersona().toLowerCase().compareTo(o1.getPersona().toLowerCase());
+        return o1.getPrioridad().toLowerCase().compareTo(o1.getPrioridad().toLowerCase());
     }
 
 }
