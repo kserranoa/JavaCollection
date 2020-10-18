10e3a12e7bbd070abd3ec445aefd1d1dfdec5072
Karina <kserranoa16@gmail.com>
2020-10-18 03:11:34 -0600

    Creado metodo lista

--- src/main/java/collection/AgregarTiquete.java
+++ src/main/java/collection/AgregarTiquete.java
@@ -1,7 +1,19 @@
 // Agrega los detalles de los tiquetes a las colecciones
 
 package collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class AgregarTiquete {
-
+   private List<InfoTiquete> ticket;
+   
+   public AgregarTiquete(){
+       ticket = new LinkedList<>();
+   }
+   //int numTiquete, String persona, String problema, String descripcion
+   public void agregaDatos(){
+        ticket.add(new InfoTiquete(1, "Karina Serrano", "Problema de equipo", "No funciona el monitor", "bajo", "soporte tecnico", "abierto"));
+        ticket.add(new InfoTiquete(2, "Yinny Najera", "Problema de equipo", "Teclado danado", "medio", "soporte tecnico", "abierto"));
+   }
+  
 }
