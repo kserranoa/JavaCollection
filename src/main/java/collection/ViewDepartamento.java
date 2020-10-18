9e32248093e6986c0c739aceb9e3296d5aca58c7
Karina <kserranoa16@gmail.com>
2020-10-18 16:30:16 -0600

    Metodos para manejo de tiquetes

--- src/main/java/collection/ViewDepartamento.java
+++ src/main/java/collection/ViewDepartamento.java
@@ -8,7 +8,7 @@ public class ViewDepartamento implements Comparator<InfoTiquete> {
     
     @Override
     public int compare(InfoTiquete o1, InfoTiquete o2) {
-        return o1.getPersona().toLowerCase().compareTo(o1.getPersona().toLowerCase());
+        return o1.getDepartamento().toLowerCase().compareTo(o1.getDepartamento().toLowerCase());
     }
 
 }
