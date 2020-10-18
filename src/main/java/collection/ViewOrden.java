6200d949b520fa475f124e338bc7f58842f20f45
Karina <kserranoa16@gmail.com>
2020-10-18 15:39:06 -0600

    Metodos para manejo de tiquetes

--- src/main/java/collection/ViewOrden.java
+++ src/main/java/collection/ViewOrden.java
@@ -2,6 +2,22 @@
 
 package collection;
 
-public class ViewOrden {
+import java.util.Comparator;
 
-}
+public class ViewOrden  implements Comparator<InfoTiquete>{
+
+    // Metodo abstracto
+    @Override
+    public int compare(InfoTiquete o1, InfoTiquete o2) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        if(o1.getNumTiquete() > o2.getNumTiquete())
+            return 1;
+        else if(o1.getNumTiquete() < o2.getNumTiquete())
+            return -1;
+        else
+            return 0;
+           
+        }
+    }
+    
+
