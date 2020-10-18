3447b94f2ac2fe7d376ebd7b3c160e2942fc697c
Karina <kserranoa16@gmail.com>
2020-10-18 16:30:20 -0600

    Metodos para manejo de tiquetes

--- src/main/java/collection/ViewEstado.java
+++ src/main/java/collection/ViewEstado.java
@@ -8,7 +8,7 @@ public class ViewEstado implements Comparator<InfoTiquete> {
     
     @Override
     public int compare(InfoTiquete o1, InfoTiquete o2) {
-        return o1.getPersona().toLowerCase().compareTo(o1.getPersona().toLowerCase());
+        return o1.getEstadoTiquete().toLowerCase().compareTo(o1.getEstadoTiquete().toLowerCase());
     }
 
 }
